8eeb92de5ae85eec62872de0b5eec03c2de8679c
isabellkaitlyn <isabell.allard@gmail.com>
2024-02-04 14:48:16 -0700

    Start Hero Born 2

---
+++ Hero Born Arena 2/Assets/Scripts/Item2Behavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item2Behavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.parent.gameObject);
+            Debug.Log("Weapon Obtained!");
+        }
+    }
+
+}
